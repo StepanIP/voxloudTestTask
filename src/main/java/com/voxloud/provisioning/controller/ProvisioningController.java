933de70481c86caae3af0cc81c2494c418604a0b
StepanIP <scrupnichuk@gmail.com>
2024-07-27 21:56:28 +0300

    feature/DEV-2_Implemented_ProvisioningController

--- src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
+++ src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
@@ -1,5 +1,10 @@
 package com.voxloud.provisioning.controller;
 
+import com.voxloud.provisioning.exception.DeviceNotFoundException;
+import com.voxloud.provisioning.service.ProvisioningService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class ProvisioningController {
 
-    // TODO Implement controller method
+    private final ProvisioningService provisioningService;
+
+    public ProvisioningController(ProvisioningService provisioningService) {
+        this.provisioningService = provisioningService;
+    }
+
+    @GetMapping("/provisioning/{macAddress}")
+    public ResponseEntity<String> provision(@PathVariable String macAddress) {
+        try {
+            String config = provisioningService.getProvisioningFile(macAddress);
+            return ResponseEntity.ok(config);
+        } catch (DeviceNotFoundException e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+    }
 }
